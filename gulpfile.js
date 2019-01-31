bc669ba7fda3def8b21c2af6995aa4f57d0bca27
mojopoly <cyrus.eslamian@gmail.com>
2019-01-31 11:42:28 -0500

    Project 2 90%

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,33 @@
-const gulp = require('gulp');
-const terser = require('gulp-terser'),
-      rename = require('gulp-rename'),
-      eslint = require('gulp-eslint'),
-      browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+    terser = require('gulp-terser'),
+    rename = require('gulp-rename'),
+    eslint = require('gulp-eslint'),
+    browserSync = require('browser-sync').create(),
+    sass = require("gulp-sass"),
+    autoprefixer = require("gulp-autoprefixer"),
+    cssnano = require("gulp-cssnano");
 
+gulp.task("sass", function() {
+    return gulp
+      .src("./Sass/scss/main.scss")
+      .pipe(sass())
+      .pipe(
+        autoprefixer({
+          browsers: ["last 2 versions"]
+        })
+      )
+      .pipe(gulp.dest("./build/css"))
+      .pipe(cssnano())
+      .pipe(rename("main.min.css"))
+      .pipe(gulp.dest("./build/css"));
+  });
+  
 gulp.task('eslint', function() {
     return gulp.src('./js/*.js')
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
-});
+})
 
 gulp.task('scripts', function(){
      return gulp
@@ -29,7 +47,6 @@ gulp.task('watch' , function() {
     gulp.watch('./*html').on("change", browserSync.reload);
 
 });
-
 gulp.task('sayHello', function(done){
     console.log('Hello world!');
     done();
--- js/main.js
+++ js/main.js
@@ -1,31 +1,54 @@
-    // $('.btn').on('click', function() {
-    //     const artistName = $('#artist-name').val();
-    //     // alert(artistName);
-    //     const artistNameIos= artistName.split(' ').join('+');
-    //     $.ajax({
-    //         method: 'GET',
-    //         url: 'https://itunes.apple.com/search?entity=album&limit=6&term=+' + artistNameIos,
-    //         success: function() {
-    //             $('.album-list').before('<p>Your request is being processed</p>')
-    //         }
-    //     })
-        
-    //     .done(function(data){
-    //         const parsedCode = JSON.parse(data);
-    //         let loopResults = parsedCode.results; 
-    //         // console.log(loopResults[0].artistId);
-    //         $.each(loopResults, function(index, value){
-    //             // console.log(key);
-    //             console.log(value); 
-    //             // console.log(albumName);
-    //             $('.album-list').append("<li>" + value.collectionName + "</li>");
-    //             // console.log(thumbailImg);
-    //             $('.album-list').append("<li>" + "<img src=" + value.artworkUrl100 + ">" + "</li>");
 
-    //     })
-    // })
-    //     // .fail(function(){
-    //     //     $('.album-list').before('There was an error with your request'); 
-    //     // })
+$(document).ready(() => {
+
+$('select').on('change', function(){
+    const subSection = $('select').val();
+    $('.loader-GIF').show();
+    $('.loader-GIF').css("display", 'flex');
+    $('.loader-GIF').css("justify-content", 'center');
+
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/topstories/v2/' + subSection + '.json?api-key=U6wOirdOtAR0f4wmxbgJNpGxWdrTfET7',
+        // success: function() {
+        //     $('select').after('<p>Your request is being processed</p>')
+        // }
+    })
+        
+    .done(function(data){
+        $('.loader-GIF').hide();
+        $('.mobile-start').css('animation-play-state', 'running');
+        $('.nav-slide').css('height', '300px');
+        let loopResults = data.results; 
+        console.log(loopResults);
+        $('.main ul').append('<ul></ul>');
+        $('.main ul').empty();
+        let titleCounter = 0
+        $.each(loopResults, function(index, value){
+            if (titleCounter <= 12 && value.multimedia.length !==0) {
+                $('.main ul').append("<li><a href=" + value.short_url + ">" + value.title 
+                + "</a> <style> a {color: #fff; text-decoration: none} </style></a></li>");
+                $('.main ul li:nth-child(' + titleCounter + ')').css('background', 'url(' + value.multimedia[4].url + ')' );
+                $('.main ul li:nth-child(' + titleCounter + ')').css('background-size', 'cover');
+                // $('.main ul li:nth-child(' + titleCounter + ')').css('background-position', 'center');
+                $('.main ul li:nth-child(' + titleCounter + ')').css('background-repeat', 'no-repeat');
+
+                titleCounter += 1;
+            }
+        })
+        $('.main li a').on('mouseenter', event => {
+            $(event.currentTarget).css("opacity", "1");
+          })
+          
+        $('.main li a').on('mouseleave', event => {
+            $(event.currentTarget).css("opacity", "0");
+          })
+    })
+        .fail(function(){
+            $('select').after('There was an error with your request'); 
+        })
     // })
 })
+
+});
+
